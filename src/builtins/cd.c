6a5dfed9349edf75dcabde1730be0da0eb973b9d
Nuno Valadão <111917125+nunovaladao@users.noreply.github.com>
2023-05-21 16:04:56 +0100

    Add files via upload

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,11 +6,37 @@
 /*   By: nsoares- <nsoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:07:46 by nsoares-          #+#    #+#             */
-/*   Updated: 2023/05/18 19:41:51 by nsoares-         ###   ########.fr       */
+/*   Updated: 2023/05/21 15:59:29 by nsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+int cd_back(t_shell *shell, t_cmds *cmds)
+{
+    cmds->cmd_line[1] = get_env("OLDPWD", shell);
+    if (cmds->cmd_line[1] == NULL) 
+    {
+        printf("minishell: cd: OLDPWD not set\n");
+        return (g_ex_status = 1);
+    }
+    cmds->oldpwd = getcwd(NULL, 0);
+    if (chdir(cmds->cmd_line[1]) != 0)
+    {
+        free(cmds->oldpwd);
+        printf("minishell: cd: ");
+		printf("%s", get_env("OLDPWD", shell));
+		printf(": No such file or directory\n");
+        return (g_ex_status = 1);
+    }
+    cmds->pwd = getcwd(NULL, 0);
+    put_var_env("PWD", cmds->pwd, shell);
+    put_var_env("OLDPWD", cmds->oldpwd, shell);
+    printf("%s\n", cmds->cmd_line[1]);
+    free(cmds->pwd);
+    free(cmds->oldpwd);
+    return (g_ex_status = 0);
+}
 
 int get_cd_home(t_shell *shell)
 {
@@ -31,6 +57,8 @@ int built_cd(t_cmds *cmds, t_shell *shell)
 {
     if (!cmds->cmd_line[1])
         return (get_cd_home(shell));
+    if ((nb_of_args(cmds) == 2) && (ft_strcmp(cmds->cmd_line[1], "-") == 0))
+        return (cd_back(shell, cmds));
     if (nb_of_args(cmds) > 2)
     {
         printf("minishell: cd: too many arguments\n");
@@ -41,9 +69,10 @@ int built_cd(t_cmds *cmds, t_shell *shell)
         cmds->oldpwd = getcwd(NULL, 0);
         if (chdir(cmds->cmd_line[1]) == -1)
         {
-            ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-			ft_putstr_fd(cmds->cmd_line[1], STDERR_FILENO);
-			ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
+            free(cmds->oldpwd);
+            ft_putstr_fd("minishell: cd: ", STDOUT_FILENO);
+			ft_putstr_fd(cmds->cmd_line[1], STDOUT_FILENO);
+			ft_putstr_fd(": No such file or directory\n", STDOUT_FILENO);
             return (g_ex_status = 1);
         }
         cmds->pwd = getcwd(NULL, 0);
