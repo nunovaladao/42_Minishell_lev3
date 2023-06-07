dac9d5e84c3a2018b259157c0e9ca23c9a6313d3
Nuno Valadão <111917125+nunovaladao@users.noreply.github.com>
2023-06-07 15:36:44 +0100

    Add files via upload

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: nsoares- <nsoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 23:36:22 by nsoares-          #+#    #+#             */
-/*   Updated: 2023/05/18 16:26:51 by nsoares-         ###   ########.fr       */
+/*   Updated: 2023/06/07 15:33:46 by nsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ int builtins(t_cmds *cmds, t_shell *shell)
 {
     if (ft_strcmp(cmds->cmd_line[0], "echo") == 0)
         built_echo(cmds);
-    else if (ft_strcmp(cmds->cmd_line[0], "x") == 0)
-        clear_screen();
     else if (ft_strcmp(cmds->cmd_line[0], "cd") == 0)
         built_cd(cmds, shell);
     else if (ft_strcmp(cmds->cmd_line[0], "pwd") == 0)
